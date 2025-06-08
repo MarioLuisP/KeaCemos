48e6802d6676b44f4ff0a866760d5f18e83a17a7
tester.passalia <tester.passalia@gmail.com>
2025-06-08 03:52:00 +0000

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
+    tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-Xlint:deprecation")
+    }
     }
 
     kotlinOptions {
@@ -37,7 +41,7 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
+    }
 
 flutter {
     source = "../.."
